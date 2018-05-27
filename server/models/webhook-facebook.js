040faa59cc4bf5c3c2c3a4737b4a1b4dafa69453
Our Academy <qpdiam@gmail.com>
2018-05-27 06:31:39 +0000

    feat: communicate facebook webhook with api and dialog flow api

--- server/models/webhook-facebook.js
+++ server/models/webhook-facebook.js
@@ -1,8 +1,44 @@
 'use strict';
 
-function processMessage(){
-    
+const axios = require('axios');
+
+const FACEBOOK_ACCESS_TOKEN = "EAAc0hfsZCeaMBANU4PSOVr3qFzBsIHAmoRkZAHJS6nq0J8HhamqZAQC3XAEmBY6zZCZA7914uyHPBCzl3CiWOfg1D3TVhmnCZBTYYHLjXt3825De0OnfpdRZCYRZCwiX6d5kgcqWSXDyS0zapTPIoFzNUVtAyzZBp7jebZAg06FQiB8QZDZD"
+const API_AI_TOKEN = 'b95db26c455c4ed681f1a7ac0f591f5f';
+
+
+const apiAiClient = require('apiai')(API_AI_TOKEN);
+
+
+const sendTextMessage = (senderId, text) => {
+    axios.post(
+        `https://graph.facebook.com/v2.6/me/messages?access_token=${FACEBOOK_ACCESS_TOKEN}`, {
+        recipient: { id: senderId },
+        message: { text },
+    })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 }
+  
+const processMessage = (event) => {
+    const senderId = event.sender.id;
+    const message = event.message.text;
+    
+    
+    const apiaiSession = apiAiClient.textRequest(message, {sessionId: 'chatbot'});
+    apiaiSession.on('response', (response) => {
+        const result = response.result.fulfillment.speech;
+        sendTextMessage(senderId, result);
+    });
+    apiaiSession.on('error', error => console.log(error));
+    apiaiSession.end();
+
+}
+
+
 
 module.exports = function(WebhookFacebook) {
     
@@ -24,18 +60,20 @@ module.exports = function(WebhookFacebook) {
      ],
     });
     
-     WebhookFacebook.handleMessage = function (req, res, cb) {
-        console.log(req)
-       /* if (req.body.object === 'page') {
+    WebhookFacebook.handleMessage = function (req, res, cb) {
+        console.log(req.body)
+        if (req.body.object === 'page') {
             req.body.entry.forEach(entry => {
                 entry.messaging.forEach(event => {
+                    console.log('event', event)
                     if (event.message && event.message.text) {
                         processMessage(event);
                     }
                 });
-        });*/
+        });
         res.status(200).end();
-    }
+       }
+     }
     
     WebhookFacebook.remoteMethod('handleMessage', {
         http: { verb: "post", path: "/"},
