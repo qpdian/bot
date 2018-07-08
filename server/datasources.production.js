12c56170d7f035fa3248bb2f8d5cb0a54787362b
Our Academy <qpdiam@gmail.com>
2018-07-08 22:31:53 +0000

    add mongo name

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -6,5 +6,9 @@ module.exports = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
+  },
+  db: {
+    name: "db",
+    connector: "memory"
   }
 };
